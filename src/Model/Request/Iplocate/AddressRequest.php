7ff2f392010feaba0597911967ac21f1d3ba0576
Velhron <velhron@gmail.com>
2020-09-22 20:42:41 +0300

    remove IplocateRequest, update AddressRequest

--- src/Model/Request/Iplocate/AddressRequest.php
+++ src/Model/Request/Iplocate/AddressRequest.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace Velhron\DadataBundle\Model\Request\Iplocate;
 
-class AddressRequest extends IplocateRequest
+use Velhron\DadataBundle\Model\Request\Suggest\SuggestRequest;
+
+class AddressRequest extends SuggestRequest
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseUrl(): string
+    {
+        return 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/';
+    }
+
     /**
      * {@inheritdoc}
      */
