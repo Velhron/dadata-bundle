e452be4347d96da0f8d72682f324249ca2bdb0e7
AlexandrErohin <alexanderErohin@yandex.ru>
2021-08-16 10:43:40 +0300

    added PhoneResponse (#11)

---
+++ src/Model/Response/Suggest/PhoneResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Velhron\DadataBundle\Model\Response\Suggest;
+
+use Velhron\DadataBundle\Traits\Phone;
+
+class PhoneResponse extends SuggestResponse
+{
+    use Phone;
+}
--- src/Traits/Email.php
+++ src/Traits/Email.php
@@ -7,6 +7,11 @@ namespace Velhron\DadataBundle\Traits;
 trait Email
 {
     /**
+     * @var string email одной строкой как в ЕГРЮЛ
+     */
+    public $source;
+
+    /**
      * @var string Локальная часть адреса (то, что до «собачки»)
      */
     public $local;
